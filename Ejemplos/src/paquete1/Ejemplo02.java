839c1feaa50c0fe0faccb0dbf150aee8a39521cb
jhandry naula <jjnaula3@utpl,edu,ec>
2024-12-11 11:43:13 -0500

    cambios

--- Ejemplos/src/paquete1/Ejemplo02.java
+++ Ejemplos/src/paquete1/Ejemplo02.java
@@ -24,18 +24,21 @@ public class Ejemplo02 {
         System.out.println(arreglo1[2]);
         System.out.println(arreglo1[3]);
         System.out.println(arreglo1[4]);
+        // impirme 0 porque no hay valores en los arreglos
         
         // arreglo de tipo double
         double[] arreglo2 = new double[5];
         System.out.println(arreglo2[0]);
+        // impime 0.0 por que es double y no hay valores que dar
         
         // arreglo de tipo String
         String[] arreglo4 = new String[5];
         System.out.println(arreglo4[0]);
-        
+        // se escribe null porque es una cadena y la cadena esta vacia 
         // arreglo de tipo boleano
         boolean[] arreglo5 = new boolean[5];
         System.out.println(arreglo5[0]);
+        // se escribe falso por que no hay valores para asignar 
     }
 
 }
